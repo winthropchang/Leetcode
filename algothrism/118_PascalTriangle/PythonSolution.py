8af7b3a7302454d71141196526e8f67000c102d2
Otto <yihao.chang@skylineww.com>
2019-05-25 23:34:19 +0800

    update: Make function more pure
    Original function execute answer and print it.
    Make lambda solution function only execute answer and return it.
    Print answer in outside.

--- algothrism/118_PascalTriangle/PythonSolution.py
+++ algothrism/118_PascalTriangle/PythonSolution.py
@@ -4,7 +4,10 @@ def lambdaSolution(level):
     # P => Pascal function
     # i => inputValue
     P=lambda i:(lambda x:x+[[a+b for a,b in zip(x[-1]+[0],[0]+x[-1])]])(P(i-1))if i>1 else[[1]]
-    print(P(level))
+    return P(level)
 
 if __name__ == "__main__":
-    lambdaSolution(5)
+    # get answer
+    answer = lambdaSolution(5)
+    
+    print(answer)
